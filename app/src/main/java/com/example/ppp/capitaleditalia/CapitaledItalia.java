c4df27c07c95865b666473496b643c0f65cad0ee
ppp <ppp@iMac-di-ppp.local>
2016-05-20 00:15:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ppp/capitaleditalia/CapitaledItalia.java
@@ -0,0 +1,56 @@
+package com.example.ppp.capitaleditalia;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CapitaledItalia extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capitaled_italia);
+
+        final Button ButtonVerifica = (Button) findViewById(R.id.buttonVerifica);
+        final Button ButtonAzzera = (Button) findViewById(R.id.buttonAzzera);
+        final TextView LabelRisposta = (TextView) findViewById(R.id.labelRisposta);
+        final EditText EditRisposta = (EditText) findViewById(R.id.editRisposta);
+
+        final String InitLabelRisposta = (String) LabelRisposta.getText();
+        final String InitEditRisposta = EditRisposta.getText().toString();
+        final String RispostaEsatta = "ROMA";
+
+        ButtonVerifica.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+
+                        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                        String RispostaData = EditRisposta.getText().toString().toUpperCase();
+                        if (RispostaData.equals(RispostaEsatta)) {
+                            LabelRisposta.setText("esatto");
+                        } else {
+                            LabelRisposta.setText("sbagliato");
+                        }
+                    }
+                }
+        );
+
+        ButtonAzzera.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        LabelRisposta.setText(InitLabelRisposta);
+                        EditRisposta.setText(InitEditRisposta);
+                    }
+                }
+        );
+    }
+}
